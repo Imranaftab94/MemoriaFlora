86e3892618c9cad4e5a0063f4d4f08778f440249
NabeelBytes6 <147080725+NabeelBytes6@users.noreply.github.com>
2024-04-20 21:03:45 +0500

    ALL DONE

--- MemoriaFlora/ViewControllers/SelectPaymentVC.swift
+++ MemoriaFlora/ViewControllers/SelectPaymentVC.swift
@@ -7,10 +7,68 @@
 
 import UIKit
 
-class SelectPaymentVC: UIViewController {
-
+class SelectPaymentVC: BaseViewController {
+    @IBOutlet weak var containerView1: UIView!
+    @IBOutlet weak var containerView3: UIView!
+    @IBOutlet weak var containerView2: UIView!
+    
+    let activeBorderColor: UIColor = UIColor.init(hexString: "#793EE5")
+    let inactiveBorderColor: UIColor = UIColor.init(hexString: "#0B0B0B")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let tapGestureRecognizer1 = UITapGestureRecognizer(target: self, action: #selector(containerViewTapped(_:)))
+        containerView1.addGestureRecognizer(tapGestureRecognizer1)
+        
+        let tapGestureRecognizer2 = UITapGestureRecognizer(target: self, action: #selector(containerViewTapped(_:)))
+        containerView2.addGestureRecognizer(tapGestureRecognizer2)
+        
+        let tapGestureRecognizer3 = UITapGestureRecognizer(target: self, action: #selector(containerViewTapped(_:)))
+        containerView3.addGestureRecognizer(tapGestureRecognizer3)
+        
+        // Set initial border colors
+        setBorderColor(for: containerView1, active: false)
+        setBorderColor(for: containerView2, active: false)
+        setBorderColor(for: containerView3, active: false)
+        
+        containerView2.layer.cornerRadius = 16.0
+        containerView2.layer.masksToBounds = true
+        
+        containerView3.layer.cornerRadius = 16.0
+        containerView3.layer.masksToBounds = true
+        
+        containerView1.layer.cornerRadius = 16.0
+        containerView1.layer.masksToBounds = true
+    }
+    
+    @IBAction func onClickPayButton(_ sender: UIButton) {
+        self.showAlert(message: "Payment Made Successfully", title: "Success", action: UIAlertAction(title: "OK", style: .default, handler: { _ in
+            self.dismiss(animated: true)
+        }))
+    }
+    
+    
+    @objc func containerViewTapped(_ sender: UITapGestureRecognizer) {
+        // Determine which container view was tapped
+        if let tappedView = sender.view {
+            // Reset border colors for all container views
+            setBorderColor(for: containerView1, active: false)
+            setBorderColor(for: containerView2, active: false)
+            setBorderColor(for: containerView3, active: false)
+            
+            // Set active border color for the tapped container view
+            setBorderColor(for: tappedView, active: true)
+        }
+    }
+    
+    func setBorderColor(for view: UIView, active: Bool) {
+        if active {
+            view.layer.borderColor = activeBorderColor.cgColor
+            view.layer.borderWidth = 2.0
+        } else {
+            view.layer.borderColor = inactiveBorderColor.cgColor
+            view.layer.borderWidth = 0.0
+        }
     }
 }
