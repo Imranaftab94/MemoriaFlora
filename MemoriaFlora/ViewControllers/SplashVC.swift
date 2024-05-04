ea98277834032ccf7fd0ac960448294773b2a932
Nabeel6236 <abdullah6236a@gmail.com>
2024-05-04 07:13:03 +0500

    pods udpate

--- MemoriaFlora/ViewControllers/SplashVC.swift
+++ MemoriaFlora/ViewControllers/SplashVC.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseCore
 
-class SplashVC: UIViewController {
+class SplashVC: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +24,9 @@ class SplashVC: UIViewController {
         self.performOperation()
     }
     
-    
-    
     private func performOperation() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
-            if let _ = MyUserDefaults.getUser(), MyUserDefaults.getRememberMe() {
+            if let _ = AppController.shared.user, MyUserDefaults.getRememberMe() {
                 // User exists, navigate to HomeViewController with animation
                 let homeVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
                 let navigationVC = UINavigationController(rootViewController: homeVC)
@@ -34,6 +36,43 @@ class SplashVC: UIViewController {
                 let loginVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
                 self.navigationController?.pushViewController(loginVC, animated: true)
             }
+        }
+    }
+    
+    private func getUserFromDB(email: String) {
+        let databaseRef = Database.database().reference()
+        
+        let query = databaseRef.child("users").queryOrdered(byChild: "email").queryEqual(toValue: email).queryLimited(toFirst: 1)
+        self.showProgressHUD()
+        query.observeSingleEvent(of: .value) { (snapshot) in
+            self.hideProgressHUD()
+            guard snapshot.exists() else {
+                print("User not found")
+                self.navigateToHome()
+                return
+            }
+            
+            if let userData = snapshot.children.allObjects.first as? DataSnapshot,
+               let userDataDict = userData.value as? [String: Any] {
+                print("User data: \(userDataDict)")
+                
+                if let isAdmin = userDataDict["admin"] as? Bool {
+                    var user = AppController.shared.user
+                    user?.admin = isAdmin
+                    AppController.shared.user = user
+                }
+            }
+            self.navigateToHome()
+        } withCancel: { (error) in
+            print("Error fetching user data: \(error.localizedDescription)")
+        }
+    }
+    
+    private func navigateToHome() {
+        DispatchQueue.main.async {
+            let homeVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+            let navigationVC = UINavigationController(rootViewController: homeVC)
+            animateTransition(to: navigationVC, view: self.view)
         }
     }
 }
